e7bc7a11d9adcac84ac0ac3a83389d7383110654
PatriciaWhitezip <patcostalonga@gmail.com>
2021-08-07 11:52:36 +0100

    Feature Timeline Structure

---
+++ src/components/TimelineItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TimelineItem = ({data}) => (
+    <div className="timeline-item">
+        <div className="timeline-item-content">
+            <span className="tag" style={{ background: data.category.color }}>
+                {data.category.tag}
+            </span>
+            <time>{data.time}</time>
+            <p>{data.text}</p>
+            {/* check if we have the data link, if yes, then continue */}
+            {data.link && <a href={data.link.url}
+            target="_blank"
+            rel="noopener noreferrer">
+                {data.link.text}
+            </a>}
+            <span className="circle"></span>
+        </div>
+    </div>
+);
+
+export default TimelineItem;
---
+++ src/data.js
@@ -0,0 +1,75 @@
+const data = [
+    {
+        text: 'Hello this is a test 1',
+        time: '14:00h',
+        category: {
+            tag: 'medium',
+            color: '#ff66ee'
+        },
+        link:{
+            url: 'https://www.patriciacostalonga.com/',
+            text: 'read more'
+        }
+    },
+    {
+        text: 'Hello this is a test 2',
+        time: '14:00h',
+        category: {
+            tag: 'medium',
+            color: '#ff66ee'
+        },
+        link:{
+            url: 'https://www.patriciacostalonga.com/',
+            text: 'read more'
+        }
+    },
+    {
+        text: 'Hello this is a test 3',
+        time: '14:00h',
+        category: {
+            tag: 'medium',
+            color: '#ff66ee'
+        },
+        link:{
+            url: 'https://www.patriciacostalonga.com/',
+            text: 'read more'
+        }
+    },
+    {
+        text: 'Hello this is a test 4',
+        time: '14:00h',
+        category: {
+            tag: 'medium',
+            color: '#ff66ee'
+        },
+        link:{
+            url: 'https://www.patriciacostalonga.com/',
+            text: 'read more'
+        }
+    },
+    {
+        text: 'Hello this is a test 5',
+        time: '14:00h',
+        category: {
+            tag: 'medium',
+            color: '#ff66ee'
+        },
+        link:{
+            url: 'https://www.patriciacostalonga.com/',
+            text: 'read more'
+        }
+    },
+    {
+        text: 'Hello this is a test 6',
+        time: '14:00h',
+        category: {
+            tag: 'medium',
+            color: '#ff66ee'
+        },
+        link:{
+            url: 'https://www.patriciacostalonga.com/',
+            text: 'read more'
+        }
+    }
+]
+export default data;
